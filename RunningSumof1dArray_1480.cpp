afb31aef7777f02013cd4167237a877fe4c3aec2
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-06-21 23:32:09 +0530

    1480. Running Sum of 1d Array

---
+++ RunningSumof1dArray_1480.cpp
@@ -0,0 +1,16 @@
+/*
+~ Author    : leetcode.com/tridib_2003/
+~ Problem   : 1480. Running Sum of 1d Array
+~ Link      : https://leetcode.com/problems/running-sum-of-1d-array/
+*/
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+
+        for (int i = 1; i < nums.size(); ++i)
+            nums[i] += nums[i - 1];
+
+        return nums;
+    }
+};
